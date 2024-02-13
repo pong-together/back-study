08ce76c803efc5742a30a9b742b9a2da77454481
yeeuniii <anna862700@gmail.com>
2024-02-13 16:15:42 +0900

    feat: nginx-(gunicorn-)django 연동 #5

--- backend/config/entrypoint.sh
+++ backend/config/entrypoint.sh
@@ -3,4 +3,5 @@ cd /code
 chmod +x manage.py
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8000
+# python3 manage.py runserver 0.0.0.0:8000
+gunicorn --bind 0.0.0.0:8000 test_postgres.wsgi:application
--- backend/srcs/test_postgres/settings.py
+++ backend/srcs/test_postgres/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
 	'accounts.apps.AccountsConfig',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
