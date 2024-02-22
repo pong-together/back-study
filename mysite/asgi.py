5d0456137a744e0e078221175475689e86f4ac6b
yeeuniii <anna862700@gmail.com>
2024-02-22 20:45:19 +0900

    feat: asgi 구현 #6

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -11,6 +11,23 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+import chats.routing
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket':
+    AuthMiddlewareStack(
+        AllowedHostsOriginValidator(
+            URLRouter(
+                chats.routing.websocket_urlpatterns
+            )
+        )
+    )
+})
