40f03727d8a5c90a19d6a3bcb4ff952f37d5f3ec
yeeuniii <anna862700@gmail.com>
2024-02-22 15:38:50 +0900

    build: 채팅 서비스 구현 환경 구축 #6

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,6 +81,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
+ASGI_APPLICATION = 'config.asgi.application'
+
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [(os.environ.get('REDIS_HOST', 'localhost'), 6379)],
+        },
+    },
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
