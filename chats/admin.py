976adcf6280e1c0f21cd5a153e3e58e02c56a65e
yeeuniii <anna862700@gmail.com>
2024-02-23 15:03:45 +0900

    feat: 채팅 앱 model migration 파일 생성 및 admin 추가 #6

--- chats/admin.py
+++ chats/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from chats import models
+
 # Register your models here.
+
+admin.site.register(models.ShopUser)
+admin.site.register(models.VisitorUser)
+admin.site.register(models.ChatRoom)
+admin.site.register(models.Message)
